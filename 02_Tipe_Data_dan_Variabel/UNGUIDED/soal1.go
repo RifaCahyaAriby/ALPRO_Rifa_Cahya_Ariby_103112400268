e55c415a691f8f3c719e8ae02c4f9d7e40cabace
rifacahyaariby <rifacahyaariby@gmail.com>
2024-10-03 21:52:49 +0700

    Add files via upload

---
+++ 02_Tipe_Data_dan_Variabel/UNGUIDED/soal1.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		satu, dua, tiga string
+		temp            string
+	)
+	fmt.Print("Masukan input string: ")
+	fmt.Scanln(&satu)
+	fmt.Print("Masukan input string: ")
+	fmt.Scanln(&dua)
+	fmt.Print("Masukan input string: ")
+	fmt.Scanln(&tiga)
+	fmt.Println("Output awal = " + satu + " " + dua + " " + tiga)
+	temp = satu
+	satu = dua
+	dua = tiga
+	tiga = temp
+	fmt.Println("Output akhir = " + satu + " " + dua + " " + tiga)
+
+	// Program diatas yaitu untuk mengacak output awal 1 2 3 menjadi tidak terurut dan menjadi output akhir
+}
