533c0f72b5245045f9f61423042e2750201697a7
Rifaaaa <rifacahyaariby@gmail.com>
2024-11-22 23:49:57 +0700

    Update parkirbayar.go

--- 10_Switch_Case/UNGUIDED/parkirbayar.go
+++ 10_Switch_Case/UNGUIDED/parkirbayar.go
@@ -3,18 +3,21 @@ package main
 import "fmt"
 
 func main() {
+	// masukan variabel dan tipe data
 	var kendaraan string
 	var durasi, bayar, tarif int
 
+	// scan masukan kendaraan dan durasi / jam
 	fmt.Print("Masukan kendaraan (Motor/Mobil/Truk): ")
 	fmt.Scan(&kendaraan)
 	fmt.Print("Masukan durasi parkir (dalam jam): ")
 	fmt.Scan(&durasi)
 
-	
+	// menyatakan jika durasi kurang dari 1 maka akan di nyatakan 1 jam
 	if durasi < 1 {
 		durasi = 1 }
 
+	// kendaraan dengan beberapa aksi 
 	switch kendaraan {
 	case "Motor" :
 		tarif = 2000
@@ -25,8 +28,8 @@ func main() {
 	default :
 		fmt.Println("Jenis kendaraan tidak valid")
 	}
-
+	// perhitungan untuk tarif dan durasi lama parkir /jam
 	bayar = tarif * durasi
-
+	// mencetak hasil jumlah tarif parkir
 	fmt.Printf("Tarif parkir: Rp %d\n", bayar)
 }
